2633623d96dcd215e269171bbbd0109073a3c274
Jaaron Cabsaban <jaaron4401@gmail.com>
2025-04-14 20:28:35 -1000

    Issue-19 commit 3
    login page styles

--- src/app/auth/loginPage/page.tsx
+++ src/app/auth/loginPage/page.tsx
@@ -1,22 +1,29 @@
 'use client';
 
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Card } from 'react-bootstrap';
 
 const LoginPage = () => (
-  <Container className="d-flex flex-column align-items-center mt-5">
-    <p>Have an account?</p>
-    <div className="d-flex gap-3">
-      <Button href="/auth/signin" variant="success">
-        Login!
-      </Button>
-    </div>
-    <p>Dont have an account?</p>
-    <div className="d-flex gap-3">
-      <Button href="/auth/signup" variant="success">
-        Sign Up!
-      </Button>
-    </div>
-  </Container>
+  <div style={{ backgroundImage: 'url(/login-background.jpg)', height: '100vh', backgroundSize: 'cover' }}>
+    <Container className="d-flex flex-column align-items-center mt-5">
+      <h1 className="text-center" style={{ color: 'white' }}>Welcome</h1>
+      <Card>
+        <Card.Body>
+          <p>Have an account?</p>
+          <div className="d-flex gap-3">
+            <Button href="/auth/signin" variant="success" className="mb-4">
+              Login!
+            </Button>
+          </div>
+          <p>Don&apos;t have an account?</p>
+          <div className="d-flex gap-3">
+            <Button href="/auth/signup" variant="success">
+              Sign Up!
+            </Button>
+          </div>
+        </Card.Body>
+      </Card>
+    </Container>
+  </div>
 );
 
 export default LoginPage;
