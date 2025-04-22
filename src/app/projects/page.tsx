fa71bd57f8b869505f2c7da1a409c8bcb17545cf
peter-bel <peterbel@hawaii.edu>
2025-04-22 00:41:03 -1000

    Revert "Update page.tsx"
    This reverts commit e6c196addc84c1293c89539819f182a1ed16531f.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -21,7 +21,7 @@ const ProjectPage = () => {
         setClubs(null);
         console.log(error);
       }
-      if (data) { // Check if data is not null
+      if (data) {
         setClubs(data);
         setFetchError(null);
       }
